337ec79fdc2711a4c44da90cfe75147346792b3a
Florence <florenceadegoke1@gmail.com>
2025-06-21 16:26:05 -0700

    I corrected WhiteNoiseMiddleware

--- florealtyproject/settings.py
+++ florealtyproject/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiserMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'florealtyproject.urls'
